f5752b4dc4978e2a0114c51dabee42ca237ce0de
Sanjeet Yadav <sanjeetyadav9212@gmail.com>
2021-06-19 01:40:52 +0530

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase";
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyAPtBsXyaxcAude3JC9OmxtWkkNXBlWjk0",
+    authDomain: "sample2-889bf.firebaseapp.com",
+    projectId: "sample2-889bf",
+    storageBucket: "sample2-889bf.appspot.com",
+    messagingSenderId: "786748673986",
+    appId: "1:786748673986:web:bdde669900a64361c4905b",
+    measurementId: "G-V69QRR7LT3"
+});
+
+const auth = firebase.auth();
+const db = firebaseConfig.firestore();
+const storage = firebase.storage();
+
+export { auth, db, storage };
